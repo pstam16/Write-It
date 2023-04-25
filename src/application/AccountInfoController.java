65ad7bc7b58c9e5c84cd407dedb986a0c2f35201
Bryan Melo <bmelo650@gmail.com>
2023-04-25 13:10:47 -0700

    New Changes MainMenu & AccountInfo
    Updated MainMenu scene & AccountInfo scene

--- src/application/AccountInfoController.java
+++ src/application/AccountInfoController.java
@@ -1,120 +1,281 @@
 package application;
 
-import java.util.*;
-
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class AccountInfoController {
-
+public class AccountInfoController extends ChangePasswordController implements Initializable {	
+	@FXML 
+	private AnchorPane personalPane;
+	@FXML 
+	private ScrollPane facultyPane;
 	@FXML
-	private Button saveButton;
-	@FXML
-	private Button exitButton;
-	@FXML
-	private TextField fullNameTextField;
+	private AnchorPane studentPane;
+	@FXML 
+	private AnchorPane changePasswordPane;
+	@FXML 
+	private VBox personalInfoVBox;
+	@FXML 
+	private VBox facultyInfoVBox;
+	@FXML 
+	private VBox studentVBox;
+	@FXML 
+	private TextField firstNameTextField;
+	@FXML 
+	private TextField lastNameTextField;
 	@FXML
 	private TextField titleTextField;
+	@FXML 
+	private TextField schoolNameTextField;
+	@FXML 
+	private TextField departmentNameTextField;
 	@FXML
-	private TextField schoolAndDepartmentTextField;
-	@FXML
-	private TextField emailAddressTextField;
+	private TextField emailTextField;
 	@FXML
 	private TextField phoneNumberTextField;
+	@FXML 
+	private ComboBox<String> semestersComboBox;
+	@FXML 
+	private ListView<String> semestersListView;
+	@FXML 
+	private Text invalidInputSemesterText;
+	@FXML 
+	private ComboBox<String> coursesComboBox;
+	@FXML 
+	private ListView<String> coursesListView;
+	@FXML 
+	private Text invalidInputCourseText;
+	@FXML 
+	private ComboBox<String> programsComboBox;
+	@FXML 
+	private ListView<String> programsListView;
+	@FXML 
+	private Text invalidInputProgramText;
+	@FXML 
+	private ComboBox<String> personalCharacComboBox;
+	@FXML 
+	private ListView<String> personalCharacListView;
+	@FXML 
+	private Text invalidInputPersonalCharacText;
+	@FXML 
+	private ComboBox<String> academicCharacComboBox;
+	@FXML 
+	private ListView<String> academicCharacListView;
 	@FXML
-	private CheckBox springCheckBox;
+	private Text invalidInputAcademicCharacText;
 	@FXML
-	private CheckBox fallCheckBox;
+	private Label currentPassLabel;
 	@FXML
-	private CheckBox summerCheckBox;
+	private Label newPassLabel;
 	@FXML
-	private TextArea coursesTextArea;
+	private Label confirmPassLabel;
 	@FXML
-	private TextArea programsTextArea;
+	private PasswordField currentPassField;
 	@FXML
-	private TextArea personalCharTextArea;
+	private PasswordField newPassField;
 	@FXML
-	private TextArea academicCharTextArea;
-
-	// When save button is pressed
-	public void saveButtonAction(ActionEvent e) throws IOException {
-		// upload data to database
-		writeToFile();
-		// return to main menu
-		SceneController sceneController = new SceneController();
-		sceneController.switchToMainMenuScene(e);
+	private PasswordField confirmPassField;
+	@FXML 
+	private Text dataSavedText;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		DatabaseManager db = new DatabaseManager();
+		// HELP, only one value is being stored
+		// Get all items from database and display in list view
+		semestersListView.getItems().addAll(db.getSingleStringVar("semesters", "semesterName").toString()); 
+		coursesListView.getItems().addAll(db.getSingleStringVar("courses", "courseName"));
+		programsListView.getItems().addAll(db.getSingleStringVar("programs", "programName"));
+		personalCharacListView.getItems().addAll(db.getSingleStringVar("characteristics", "description", 0));
+		academicCharacListView.getItems().addAll(db.getSingleStringVar("characteristics", "description", 1));
+		
+		// Set items into ComboBox
+		semestersComboBox.setItems(FXCollections.observableArrayList("Fall", "Spring", "Summer", "Winter"));
+		coursesComboBox.setItems(FXCollections.observableArrayList("CS 146 - Data Structures and Algorithms", "CS 147 - Computer Architecture",
+						"CS 149 - Operating Systems", "CS 151 - Object-Oriented Design", "CS 152 - Programming Paradigms", "CS 154 - Formal Languages and Computability",
+						"CS 160 - Software Engineering", "CS 157A - Introduction to Database Management Systems", "CS 166 - Information Security"));
+		programsComboBox.setItems(FXCollections.observableArrayList("Advertising, BS", "Chemistry, BA", "Computer Science, BS", "Electrical Engineering, BS",
+						"Accountancy, MS", "Aerospace Engineering, MS", "Computer Science, MS", "Mathematics, MS", "Audiology, AUD", "Educational Leadership, EdD"));
+		personalCharacComboBox.setItems(FXCollections.observableArrayList("very passionate", "very enthusiastic", "punctual", "attentive", "polite"));
+		academicCharacComboBox.setItems(FXCollections.observableArrayList("submitted well-written assignments", "participated in all of my class activities",
+						"worked hard", "was very well prepared for every exam and assignment", "picked up new skills quickly", "was able to excel academically at the top of my class"));
+		
+		dataSavedText.setText("");
+	}
+	
+	// Add items selected items from combo box to list view
+	public void addSelectedSemester(ActionEvent event) {
+		if (semestersComboBox.getValue() != null) {
+			semestersListView.getItems().add(semestersComboBox.getValue());
+			invalidInputSemesterText.setText("");
+		} else {
+			invalidInputSemesterText.setText("Invalid input");
+		}
+	}
+	
+	public void addSelectedCourse(ActionEvent event) {
+		if (coursesComboBox.getValue() != null) {
+			coursesListView.getItems().add(coursesComboBox.getValue());
+			invalidInputCourseText.setText("");
+		} else {
+			invalidInputCourseText.setText("Invalid input");
+		}
+	}
+	
+	public void addSelectedProgram(ActionEvent event) {
+		if (programsComboBox.getValue() != null) {
+			programsListView.getItems().add(programsComboBox.getValue());
+			invalidInputProgramText.setText("");
+		} else {
+			invalidInputProgramText.setText("Invalid input");
+		}
 	}
 
-	// When exit button is pressed
-	// Return to main menu
-	public void exitButtonAction(ActionEvent e) throws IOException {
-		SceneController sceneController = new SceneController();
-		sceneController.switchToMainMenuScene(e);
+	public void addSelectedPersonalCharac(ActionEvent event) {
+		if (personalCharacComboBox.getValue() != null) {
+			personalCharacListView.getItems().add(personalCharacComboBox.getValue());
+			invalidInputPersonalCharacText.setText("");
+		} else {
+			invalidInputPersonalCharacText.setText("Invalid input");
+		}
 	}
 
-	// Save to file
+	public void addSelectedAcademicCharac(ActionEvent event) {
+		if (academicCharacComboBox.getValue() != null) {
+			academicCharacListView.getItems().add(academicCharacComboBox.getValue());
+			invalidInputAcademicCharacText.setText("");
+		} else {
+			invalidInputAcademicCharacText.setText("Invalid input");
+		}
+	}
+
+	// Remove selected items from view
+	public void removeSelectedSemester(ActionEvent event) {
+		int selectedID = semestersListView.getSelectionModel().getSelectedIndex();
+		semestersListView.getItems().remove(selectedID);
+	}
+	
+	public void removeSelectedCourse(ActionEvent event) {
+		int selectedID = coursesListView.getSelectionModel().getSelectedIndex();
+		coursesListView.getItems().remove(selectedID);
+	}
+	
+	public void removeSelectedProgram(ActionEvent event) {
+		int selectedID = programsListView.getSelectionModel().getSelectedIndex();
+		programsListView.getItems().remove(selectedID);
+	}
+	
+	public void removeSelectedPersonalCharac(ActionEvent event) {
+		int selectedID = personalCharacListView.getSelectionModel().getSelectedIndex();
+		personalCharacListView.getItems().remove(selectedID);
+	}
+
+	public void removeSelectedAcademicCharac(ActionEvent event) {
+		int selectedID = academicCharacListView.getSelectionModel().getSelectedIndex();
+		academicCharacListView.getItems().remove(selectedID);
+	}
+	
+	// When new pane is selected from secondary side-menu's buttons
+	public void personalPaneSwitch(MouseEvent e) {
+		personalPane.setVisible(true);
+		facultyPane.setVisible(false);
+		studentPane.setVisible(false);
+		changePasswordPane.setVisible(false);
+	}
+
+	public void facultyPaneSwitch(MouseEvent e) {
+		personalPane.setVisible(false);
+		facultyPane.setVisible(true);
+		studentPane.setVisible(false);
+		changePasswordPane.setVisible(false);
+	}
+
+	public void studentPaneSwitch(MouseEvent e) {
+		personalPane.setVisible(false);
+		facultyPane.setVisible(false);
+		studentPane.setVisible(true);
+		changePasswordPane.setVisible(false);
+	}
+	
+	public void changePasswordPaneSwitch(MouseEvent event) {
+		personalPane.setVisible(false);
+		facultyPane.setVisible(false);
+		studentPane.setVisible(false);
+		changePasswordPane.setVisible(true);
+	}
+	
+	// When "Edit" button is pressed
+	public void enablePersonalInfoVBox(MouseEvent event) {
+		personalInfoVBox.setDisable(false);
+	}
+	
+	public void enableFacultyVBox(MouseEvent event) {
+		facultyInfoVBox.setDisable(false);
+	}
+	
+	public void enableStudentVBox(MouseEvent event) {
+		studentVBox.setDisable(false);
+	}
+	
+	// When the change password button is pressed
+	public void changePassButtonAction(ActionEvent e) throws IOException {
+		super.changePassButtonAction(e);
+	}
+
+	// Displays error message if: Password fields are blank, Current password is wrong, or New and confirm passwords do not match
+	public void passButtonError(String currentPassword, String newPassword, String confirmPassword) {
+		super.passButtonError(currentPassword, newPassword, confirmPassword);
+	}
+
+	// Checks to see if confirm password matches with new password
+	public boolean passwordConfirmation(String newPassword, String confirmPassword) {
+		return super.passwordConfirmation(newPassword, confirmPassword);
+	}
+	
 	public void writeToFile() {
 		// object to control database operations
 		DatabaseManager db = new DatabaseManager();
 
-		// Scanner objects to read data from text areas
-		Scanner coursesObj = new Scanner(coursesTextArea.getText());
-		Scanner programsObj = new Scanner(programsTextArea.getText());
-		Scanner personalCharObj = new Scanner(personalCharTextArea.getText());
-		Scanner academicCharObj = new Scanner(academicCharTextArea.getText());
-
 		// List objects to hold data from text areas
+		List<String> semestersList = new ArrayList<>();
 		List<String> coursesList = new ArrayList<>();
 		List<String> programsList = new ArrayList<>();
 		List<String> personalCharList = new ArrayList<>();
 		List<String> academicCharList = new ArrayList<>();
-		List<String> semestersList = new ArrayList<>();
-
-		String name = fullNameTextField.getText();
+		
+		// Add all inputs from personal info page
+		String name = firstNameTextField.getText() + " " + lastNameTextField.getText();
 		String title = titleTextField.getText();
-		String schoolDepartment = schoolAndDepartmentTextField.getText();
-		String email = emailAddressTextField.getText();
+		String schoolDepartment = schoolNameTextField.getText() + " " + departmentNameTextField.getText();
+		String email = emailTextField.getText();
 		String phoneNumber = phoneNumberTextField.getText();
-
-		if (springCheckBox.isSelected()) {
-			semestersList.add("Spring\n");
-		}
-		if (fallCheckBox.isSelected()) {
-			semestersList.add("Fall\n");
-		}
-		if (summerCheckBox.isSelected()) {
-			semestersList.add("Summer\n");
-		}
-
-		try {
-			// add user inputs to ArrrayLists for database upload
-			while (coursesObj.hasNextLine()) {
-				coursesList.add(coursesObj.nextLine() + "\n");
-			}
-
-			while (programsObj.hasNextLine()) {
-				programsList.add(programsObj.nextLine() + "\n");
-			}
-
-			while (personalCharObj.hasNextLine()) {
-				personalCharList.add(personalCharObj.nextLine() + "\n");
-			}
-
-			while (academicCharObj.hasNextLine()) {
-				academicCharList.add(academicCharObj.nextLine() + "\n");
-			}
-
-		} catch (Exception d) {
-			System.out.println("An error occurred.");
-			d.printStackTrace();
-		}
-
+		
+		// Add all items from faculty info page
+		semestersList.addAll(semestersListView.getItems());
+		coursesList.addAll(coursesListView.getItems());
+		programsList.addAll(programsListView.getItems());
+		personalCharList.addAll(personalCharacListView.getItems());
+		academicCharList.addAll(academicCharacListView.getItems());
+		
 		// Upload user data to database
 		if (!name.isEmpty() && !title.isEmpty() && !schoolDepartment.isEmpty() && !email.isEmpty()
 				&& !phoneNumber.isEmpty()) {
@@ -122,7 +283,7 @@ public class AccountInfoController {
 		}
 		// upload semesters, courses, programs, personal characteristics,
 		// academic characteristics to database
-		Iterator<String> iter = semestersList.iterator();
+		Iterator<String> iter = semestersList.listIterator();
 		while (iter.hasNext()) {
 			// System.out.println(iter.next());
 			db.setSingleStringVar("semesters", "semesterName", iter.next());
@@ -149,6 +310,42 @@ public class AccountInfoController {
 		while (iter.hasNext()) {
 			db.setSingleStringVarWithType("characteristics", "description", iter.next(), 1);
 		}
+
 	}
 
+	// When home button is pressed
+		// Return to main menu screen
+		public void homeButtonAction(ActionEvent e) throws IOException {
+			SceneController sceneController = new SceneController();
+			sceneController.switchToMainMenuScene(e);
+		}
+		
+		// When account button is pressed
+		// Refresh current page
+		public void accountButtonAction(ActionEvent e) throws IOException {
+			SceneController sceneController = new SceneController();
+			sceneController.switchToAccountInfoScene(e);
+		}
+
+		// When logout button is pressed
+		// Return to log-in screen
+		public void logoutButtonAction(ActionEvent e) throws IOException {
+			SceneController sceneController = new SceneController();
+			sceneController.switchToLoginScene(e);
+		}
+
+		// When save button is pressed
+		// Save data to database and return to home screen
+		public void saveButtonAction(ActionEvent e) throws IOException {
+			// upload data to database
+			writeToFile();
+			dataSavedText.setText("Information has been updated");
+		}
+
+		// When exit button is pressed
+		// Return to main menu
+		public void exitButtonAction(ActionEvent e) throws IOException {
+			SceneController sceneController = new SceneController();
+			sceneController.switchToMainMenuScene(e);
+		}
 }
--- src/application/MainMenuController.java
+++ src/application/MainMenuController.java
@@ -1,13 +1,22 @@
 package application;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
 
-public class MainMenuController {
+public class MainMenuController implements Initializable {
 
+	@FXML 
+	private Button homeButton;
+	@FXML
+	private Button accountButton;
 	@FXML
 	private Button logoutButton;
 	@FXML
@@ -16,25 +25,71 @@ public class MainMenuController {
 	private Button createRecommendationButton;
 	@FXML
 	private Button searchRecommendationButton;
-	@FXML
-	private Button accountButton;
-	@FXML
+	@FXML 
 	private Button editButton;
-	@FXML
+	@FXML 
 	private Button deleteRecommendationButton;
+	@FXML 
+	private Text welcomeMessage;
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		String name = getName();
+		
+		if (!name.isEmpty()) {
+			welcomeMessage.setText(getTime() + name);
+		}
+		else {
+			welcomeMessage.setText(getTime());
+		}
+	}
+	
+	// Returns user's name stored in database
+	public String getName() {
+		try {
+			DatabaseManager db = new DatabaseManager();
+			String s = db.getSingleStringVar("user", "name").toString();
+			String[] name = s.split(" ");
+			return name[0];
+		}  catch (Exception d) {
+			System.out.println("An error occurred.");
+			d.printStackTrace();
+			return "";
+		}
+	}
+	
+	// Gets current time of device
+	// Used to display proper greeting of the day on main menu
+	public String getTime() {
+		LocalTime morning = LocalTime.parse("00:00:00");
+		LocalTime noon = LocalTime.parse("12:00:00");
+		LocalTime evening = LocalTime.parse("18:00:00");
+		LocalTime time = LocalTime.now();
+		
+		if (time.isAfter(morning) && time.isBefore(noon)) {
+			return "Good morning,\n";
+		}
+		else if (time.isAfter(noon) && time.isBefore(evening)) {
+			return "Good afternoon,\n";
+		}
+		else {
+			return "Good evening,\n";
+		}
+	}
+	
+	// When home button is pressed
+	// Used to refresh home page
+	public void homeButtonAction(ActionEvent e) throws IOException {
+		SceneController sceneController = new SceneController();
+		sceneController.switchToMainMenuScene(e);
+	}
 
 	// When logout button is pressed
-	// Return to main menu
+	// Return to log-in screen
 	public void logoutButtonAction(ActionEvent e) throws IOException {
 		SceneController sceneController = new SceneController();
 		sceneController.switchToLoginScene(e);
-	}
-
-	// When change password button is pressed
-	// Redirect to change password
-	public void changePassButtonAction(ActionEvent e) throws IOException {
-		SceneController sceneController = new SceneController();
-		sceneController.switchToChangePasswordScene(e);
 	}
 
 	// When create recommendation button is pressed
@@ -52,7 +107,7 @@ public class MainMenuController {
 	}
 
 	// Switch to account info scene
-	public void accountButtonAction(ActionEvent e) throws IOException {
+	public void accountButtonAction(ActionEvent e) throws IOException {	
 		SceneController sceneController = new SceneController();
 		sceneController.switchToAccountInfoScene(e);
 	}
@@ -64,4 +119,5 @@ public class MainMenuController {
 	public void deleteButtonAction(ActionEvent event) {
 		// TODO:
 	}
+	
 }
